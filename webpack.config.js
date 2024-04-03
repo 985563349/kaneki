3eded6556d9db8cd5039a40f00e88e28ebd73128
王杰 <jie985563349@outlook.com>
2024-04-03 14:15:12 +0800

    build: initialize project

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'kaneki.js',
+    library: {
+      type: 'commonjs2',
+    },
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
